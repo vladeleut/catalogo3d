b70b6ea1b36cb7af83b1eed34762d72f98d188a4
Vlademir Eleuterio <vla.eleut@gmail.com>
2025-12-07 21:59:11 -0300

    Adiciona muitos produtos.

--- js/produtos.js
+++ js/produtos.js
@@ -8,16 +8,64 @@ const PRODUTOS = [
 		nome: "Cubo infinito Antiestresse",
 		preco: 10,
 		descricao: "Cubo infinito antiestresse com várias faces para manipular.",
-		categorias: ["Antiestresse","destaque"],
+		categorias: ["Antiestresse","Destaque"],
 		imagens: ["0061.jpg","0062.jpg","0063.jpg"]
+	},
+	{
+		id: 9,
+		nome: "Quebra Cabeça 54 peças",
+		preco: 45,
+		descricao: "Quebra cabeça com 54 peças, desafio divertido para todas as idades. Encaixe todas as peças para completar o cubo. Acompanha base e suporte.",
+		categorias: ["Diversão","Decoração","Presente","Destaque"],
+		imagens: ["0091.jpg", "0092.jpg"]
 	},
 	{
 		id: 1,
 		nome: "Vaso Porta-Copos",
-		preco: 40,
+		preco: 39.90,
 		descricao: "Porta Copos decorativo em formato de vaso.",
 		categorias: ["Organização","Decoração","Presente"],
 		imagens: ["0011.jpg", "0012.jpg"]
+	},
+	{
+		id: 3,
+		nome: "Spinner Planetário Antiestresse",
+		preco: 10,
+		descricao: "Spinner planetário com engrenagens antiestresse.",
+		categorias: ["Antiestresse"],
+		imagens: ["0032.gif","0031.jpg"]
+	},
+	{
+		id: 5,
+		nome: "Descanso de Copos Harry Potter",
+		preco: 25,
+		descricao: "Descanso de copos com tema Harry Potter.",
+		categorias: ["Organização","Presente"],
+		imagens: ["0051.jpg","0052.jpg","0053.jpg"]
+	},
+	{
+		id: 7,
+		nome: "Dispenser Cápsular Nespresso Pro",
+		preco: 29.90,
+		descricao: "Dispenser para cápsulas Nespresso Pro, organiza e decora. Comporta 19 cápsulas.",
+		categorias: ["Organização","Decoração","destaque"],
+		imagens: ["0071.jpg","0072.jpg","0073.jpg"]
+	},
+	{
+		id: 8,
+		nome: "Spinner Giratório Antiestresse",
+		preco: 6,
+		descricao: "Spinner giratório, ideal para aliviar o estresse.",
+		categorias: ["Antiestresse","Brinquedo","Presente"],
+		imagens: ["0083.gif","0081.jpg","0082.jpg"]
+	},
+	{
+		id: 10,
+		nome: "Suporte Pequeno para Celular",
+		preco: 5,
+		descricao: "Suporte pequeno e compacto para celular, mantém o aparelho em posição confortável sobre a mesa.",
+		categorias: ["Organização","Acessório","Presente"],
+		imagens: ["0101.jpg","0102.jpg","0103.jpg","0104.jpg"]
 	},
 	{
 		id: 2,
@@ -28,14 +76,6 @@ const PRODUTOS = [
 		imagens: ["0021.jpg","0022.jpg","0023.jpg", "0024.jpg"]
 	},
 	{
-		id: 3,
-		nome: "Spinner Planetário Antiestresse",
-		preco: 10,
-		descricao: "Spinner planetário com engrenagens antiestresse.",
-		categorias: ["Antiestresse"],
-		imagens: ["0031.jpg"]
-	},
-	{
 		id: 4,
 		nome: "Chaveiro Mini Antiestresse hexágono",
 		preco: 4,
@@ -44,11 +84,27 @@ const PRODUTOS = [
 		imagens: ["0041.jpg","0042.jpg","0043.jpg"]
 	},
 	{
-		id: 5,
-		nome: "Descanso de Copos Harry Potter",
-		preco: 25,
-		descricao: "Descanso de copos com tema Harry Potter.",
-		categorias: ["Organização","Presente"],
-		imagens: ["0051.jpg","0052.jpg","0053.jpg"]
+		id: 11,
+		nome: "Mini arremessador de discos",
+		preco: 10,
+		descricao: "Mini arremessador de discos. Acompanha 3 discos para se divertir com seu pet. Ideal para gatos.",
+		categorias: ["Pet","Presente","Diversão"],
+		imagens: ["0112.gif","0111.jpg"]
+	},
+	{
+		id: 12,
+		nome: "Combo mini-discos para mini arremessador de discos",
+		preco: 4,
+		descricao: "Combo com 5 mini discos para reposição do mini arremessador de discos. Ideal para gatos.",
+		categorias: ["Pet","Presente","Diversão"],
+		imagens: ["0121.jpg"]
+	},
+	{
+		id: 13,
+		nome: "Triângulo articulado infinito",
+		preco: 12,
+		descricao: "Triângulo articulado infinito antiestresse. Manipule o triângulo para aliviar o estresse e se divertir.",
+		categorias: ["Antiestresse"],
+		imagens: ["0131.jpg","0132.jpg","0133.jpg"]
 	}
 ];
